c9278a213cd7d1fa108d9678858b9a193240fed4
Nhuong <www.nhuong.zzz@gmail.com>
2022-05-14 17:34:07 +0700

    todo

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 }); 
 
 app.use((req, res, next) => {
-  console.log("đây là middlewe2")
+  res.send('<h1>Hello from Express.js</h1>')
 }); 
 
 const server = http.createServer(app)
