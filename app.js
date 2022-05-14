e1c8ae88041e5c081bdf9b12470acdaed0df38c3
Nhuong <www.nhuong.zzz@gmail.com>
2022-05-14 17:27:48 +0700

    todo

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require("express")
 
 const app = express()
 
+app.use((req, res, next) => {
+  console.log("đây là middlewe")
+  next()
+}); 
+
+app.use((req, res, next) => {
+  console.log("đây là middlewe2")
+}); 
+
 const server = http.createServer(app)
 
 server.listen(3000);
