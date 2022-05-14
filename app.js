4aa6eb80ce9770b2ef29ef6b7f7542744f5f4cf5
Nhuong <www.nhuong.zzz@gmail.com>
2022-05-14 17:59:27 +0700

    todo

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const http = require("http");
+const express = require("express");
 
-const express = require("express")
+const port = 3000;
 
-const app = express()
+const app = express();
 
-app.use((req, res, next) => {
-  console.log("đây là middlewe")
-  next()
-}); 
+app.use("/", (req, res, next) => {
+  console.log("hello from Express.js");
+  next();
+});
 
-app.use((req, res, next) => {
-  res.send('<h1>Hello from Express.js</h1>')
-}); 
+app.use("/add-product", (req, res, next) => {
+  res.send("<h1>The Add product page</h1>");
+});
 
-const server = http.createServer(app)
-
-server.listen(3000);
+app.use('/',(req, res, next) => {
+  res.send("<h1>Hello from Express.js</h1>");
+});
+app.listen(port);
