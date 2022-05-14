8e9dde08df418d2cbfcc51db8031d3f664591cad
Nhuong <www.nhuong.zzz@gmail.com>
2022-05-14 18:31:49 +0700

    todo

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const port = 3000;
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("hello from Express.js");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
-  res.send("<h1>The Add product page</h1>");
+  res.send(
+    '<form action="/product" method="POST" ><input type="text" name="title" /><button type="submit" >Add product</button> </form>'
+  );
 });
 
-app.use('/',(req, res, next) => {
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express.js</h1>");
 });
 app.listen(port);
