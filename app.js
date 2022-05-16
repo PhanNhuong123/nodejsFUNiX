d211284da345bbc30a884e86a07ec540612894e4
Nhuong <www.nhuong.zzz@gmail.com>
2022-05-16 14:50:52 +0700

    todo

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.use('/admin',adminData.router);
 app.use(shopRoutes)
 
 app.use((req , res, next ) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.status(404).render('404')
 })
 
 app.listen(port);
